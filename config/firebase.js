0a568c53eb0ab37d4a32082b805e9685e8897e89
BukTon <buk_ton2@hotmail.com>
2017-06-24 00:27:22 +0700

    add firebase init

---
+++ config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyA8ZvU2B4Ed_mii92qU0593Q1sg9-Uejuk",
+  authDomain: "moosteakdeveloper.firebaseapp.com",
+  databaseURL: "https://moosteakdeveloper.firebaseio.com",
+  projectId: "moosteakdeveloper",
+  storageBucket: "moosteakdeveloper.appspot.com",
+  messagingSenderId: "85465025350"
+}
