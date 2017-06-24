bc8ed07944af4cfa97d6c6db869daf1149672c7b
bukton <buk_ton2@hotmail.com>
2017-06-24 18:14:38 +0700

    new login

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import account from '../../actions/accountAction'
+import { Link } from 'react-router'
 
 export default class App extends Component {
   render () {
     return (
       <div>
+        <Link to='/dashboard/setting'>dfsdfsdf</Link>
         <button onClick={account.SignOut} >logout</button>
         {this.props.children}
       </div>
--- src/components/account/Signin.js
+++ src/components/account/Signin.js
@@ -1,34 +1,19 @@
 import React, { Component } from 'react'
 import account from '../../actions/accountAction'
-import { Button, Grid, Row, Col, Image, Glyphicon} from 'react-bootstrap';
-
 
 class Signin extends Component {
   render () {
     return (
         <div className="login">
-            <Grid>
-                <Row>
-                    <Col xs={12} className="img-logo">
-                        <Image className="img-size" src="../../static/asset/images/384.png" rounded />
-                    </Col>
-                </Row>
-                <Row>
-                    <div className="btn-facebook">
-                        <Col sm={12}>
-                            <Button className="btn-login" block bsStyle="primary" onClick={account.SigeInFacebook}>Login with Facebook</Button>
-                        </Col>
-                    </div>
-                </Row>
-                <Row>
-                    <Col sm={12}>
-                        <div className="btn-google">
-                            <Button className="btn-login" block bsStyle="danger"  onClick={account.SigeInGoogle} >Login with Google</Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Grid>
-         {/*<Button onClick={account.SignOut} >logout</Button>*/}
+            <div className="col-sm-4 col-sm-offset-4 text-center" style={{marginTop: '5%'}} >
+                <img className="img-responsive" style={{height: 250, margin: 'auto'}} src="/static/asset/images/384.png" />
+            </div>
+            <div className='col-sm-2 col-md-4 col-sm-offset-5 col-md-offset-4 text-center' style={{marginTop: 100}}>
+                <button className="btn btn-primary btn-lg "  style={{width: 300}} onClick={account.SigeInFacebook}><i className='fa fa-facebook' />&nbsp;&nbsp;&nbsp;Login with Facebook</button>
+            </div>
+            <div className='col-sm-2 col-md-4 col-sm-offset-5 col-md-offset-4 text-center' style={{marginTop: 30}}>
+                <button className="btn btn-danger btn-lg "  style={{width: 300}} onClick={account.SigeInGoogle}><i className='fa fa-google' />&nbsp;&nbsp;&nbsp;Login with Google</button>
+            </div>
         </div>
     )
   }
