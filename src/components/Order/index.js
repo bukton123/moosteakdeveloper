a27ccd599b8ad98fd2b2f353d2afece53da4c097
Lalida Khammong <lalida@digio.co.th>
2017-06-25 04:46:44 +0700

    Add Order menu

---
+++ src/components/Order/index.js
@@ -0,0 +1,261 @@
+import React, { Component } from 'react'
+
+class Order extends Component {
+    render () {
+      return (
+        <div>
+          <h2>รายการอาหาร</h2>
+          <div className='row'>
+            <div className='col-lg-4 col-sm-12'>
+              <div className='card analytic-user'>
+                <div className='card-block-big'>
+                  <div className='col-xs-6'>
+                    <div className='numbers'>
+                      <h3>หมูสะเต๊ะ</h3>
+                      <h2>ชุดละ 50 บาท</h2>
+                       <span className='text-warning star'><i className='fa fa-star' /><i className='fa fa-star' /><i className='fa fa-star' /></span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='icon-big icon-warning text-center'>
+                      <img src='/static/asset/images/moo.jpg' alt='moosteak' />
+                    </div>
+                  </div>
+                 
+                </div>
+                <div className='card-footer'>
+                  <div className='col-xs-6'>
+                     <div className='input-group'>
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' disabled='disabled' data-type='minus'>
+                                <span className='glyphicon glyphicon-minus'></span>
+                            </button>
+                        </span>
+                        <input type='text' className='form-control input-number' value='1' min='1' max='10' />
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' data-type='plus' data-field='quant[1]'>
+                                <span className='glyphicon glyphicon-plus'></span>
+                            </button>
+                        </span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='numbers right'>
+                      <button className='btn btn-new-warning-view btn-flat-lg-login' style={{width: '100%'}}>Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 col-sm-12'>
+              <div className='card analytic-user'>
+                <div className='card-block-big'>
+                  <div className='col-xs-6'>
+                    <div className='numbers'>
+                      <h3>หมูสะเต๊ะ</h3>
+                      <h2>ชุดละ 50 บาท</h2>
+                       <span className='text-warning star'><i className='fa fa-star' /><i className='fa fa-star' /><i className='fa fa-star' /></span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='icon-big icon-warning text-center'>
+                      <img src='/static/asset/images/moo.jpg' alt='moosteak' />
+                    </div>
+                  </div>
+                 
+                </div>
+                <div className='card-footer'>
+                  <div className='col-xs-6'>
+                     <div className='input-group'>
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' disabled='disabled' data-type='minus'>
+                                <span className='glyphicon glyphicon-minus'></span>
+                            </button>
+                        </span>
+                        <input type='text' className='form-control input-number' value='1' min='1' max='10' />
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' data-type='plus' data-field='quant[1]'>
+                                <span className='glyphicon glyphicon-plus'></span>
+                            </button>
+                        </span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='numbers right'>
+                      <button className='btn btn-new-warning-view btn-flat-lg-login' style={{width: '100%'}}>Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 col-sm-12'>
+              <div className='card analytic-user'>
+                <div className='card-block-big'>
+                  <div className='col-xs-6'>
+                    <div className='numbers'>
+                      <h3>หมูสะเต๊ะ</h3>
+                      <h2>ชุดละ 50 บาท</h2>
+                       <span className='text-warning star'><i className='fa fa-star' /><i className='fa fa-star' /><i className='fa fa-star' /></span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='icon-big icon-warning text-center'>
+                      <img src='/static/asset/images/moo.jpg' alt='moosteak' />
+                    </div>
+                  </div>
+                 
+                </div>
+                <div className='card-footer'>
+                  <div className='col-xs-6'>
+                     <div className='input-group'>
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' disabled='disabled' data-type='minus'>
+                                <span className='glyphicon glyphicon-minus'></span>
+                            </button>
+                        </span>
+                        <input type='text' className='form-control input-number' value='1' min='1' max='10' />
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' data-type='plus' data-field='quant[1]'>
+                                <span className='glyphicon glyphicon-plus'></span>
+                            </button>
+                        </span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='numbers right'>
+                      <button className='btn btn-new-warning-view btn-flat-lg-login' style={{width: '100%'}}>Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 col-sm-12'>
+              <div className='card analytic-user'>
+                <div className='card-block-big'>
+                  <div className='col-xs-6'>
+                    <div className='numbers'>
+                      <h3>หมูสะเต๊ะ</h3>
+                      <h2>ชุดละ 50 บาท</h2>
+                       <span className='text-warning star'><i className='fa fa-star' /><i className='fa fa-star' /><i className='fa fa-star' /></span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='icon-big icon-warning text-center'>
+                      <img src='/static/asset/images/moo.jpg' alt='moosteak' />
+                    </div>
+                  </div>
+                 
+                </div>
+                <div className='card-footer'>
+                  <div className='col-xs-6'>
+                     <div className='input-group'>
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' disabled='disabled' data-type='minus'>
+                                <span className='glyphicon glyphicon-minus'></span>
+                            </button>
+                        </span>
+                        <input type='text' className='form-control input-number' value='1' min='1' max='10' />
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' data-type='plus' data-field='quant[1]'>
+                                <span className='glyphicon glyphicon-plus'></span>
+                            </button>
+                        </span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='numbers right'>
+                      <button className='btn btn-new-warning-view btn-flat-lg-login' style={{width: '100%'}}>Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 col-sm-12'>
+              <div className='card analytic-user'>
+                <div className='card-block-big'>
+                  <div className='col-xs-6'>
+                    <div className='numbers'>
+                      <h3>หมูสะเต๊ะ</h3>
+                      <h2>ชุดละ 50 บาท</h2>
+                       <span className='text-warning star'><i className='fa fa-star' /><i className='fa fa-star' /><i className='fa fa-star' /></span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='icon-big icon-warning text-center'>
+                      <img src='/static/asset/images/moo.jpg' alt='moosteak' />
+                    </div>
+                  </div>
+                 
+                </div>
+                <div className='card-footer'>
+                  <div className='col-xs-6'>
+                     <div className='input-group'>
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' disabled='disabled' data-type='minus'>
+                                <span className='glyphicon glyphicon-minus'></span>
+                            </button>
+                        </span>
+                        <input type='text' className='form-control input-number' value='1' min='1' max='10' />
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' data-type='plus' data-field='quant[1]'>
+                                <span className='glyphicon glyphicon-plus'></span>
+                            </button>
+                        </span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='numbers right'>
+                      <button className='btn btn-new-warning-view btn-flat-lg-login' style={{width: '100%'}}>Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-lg-4 col-sm-12'>
+              <div className='card analytic-user'>
+                <div className='card-block-big'>
+                  <div className='col-xs-6'>
+                    <div className='numbers'>
+                      <h3>หมูสะเต๊ะ</h3>
+                      <h2>ชุดละ 50 บาท</h2>
+                       <span className='text-warning star'><i className='fa fa-star' /><i className='fa fa-star' /><i className='fa fa-star' /></span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='icon-big icon-warning text-center'>
+                      <img src='/static/asset/images/moo.jpg' alt='moosteak' />
+                    </div>
+                  </div>
+                 
+                </div>
+                <div className='card-footer'>
+                  <div className='col-xs-6'>
+                     <div className='input-group'>
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' disabled='disabled' data-type='minus'>
+                                <span className='glyphicon glyphicon-minus'></span>
+                            </button>
+                        </span>
+                        <input type='text' className='form-control input-number' value='1' min='1' max='10' />
+                        <span className='input-group-btn'>
+                            <button type='button' className='btn btn-default btn-number' data-type='plus' data-field='quant[1]'>
+                                <span className='glyphicon glyphicon-plus'></span>
+                            </button>
+                        </span>
+                    </div>
+                  </div>
+                  <div className='col-xs-6'>
+                    <div className='numbers right'>
+                      <button className='btn btn-new-warning-view btn-flat-lg-login' style={{width: '100%'}}>Add to cart</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+}
+
+export default Order
