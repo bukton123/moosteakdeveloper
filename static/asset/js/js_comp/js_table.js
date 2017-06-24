8935a4d9f2bca7d22f1dbb221dc989c5073e01d0
Lalida Khammong <lalida@digio.co.th>
2017-06-24 13:14:21 +0700

    css structure

---
+++ static/asset/js/js_comp/js_table.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#example').DataTable({
+        "pagingType": "full_numbers",
+        "bFilter": false,
+        "bLengthChange": false,
+        "sDom": 'itp'
+    });
+});
