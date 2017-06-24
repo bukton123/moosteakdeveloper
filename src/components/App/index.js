f5d7576d5fe94c64edfe4d7ca227990486309a7d
Lalida Khammong <lalida@digio.co.th>
2017-06-24 18:04:45 +0700

    update navbar link

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,15 +5,10 @@ export default class App extends Component {
   render () {
     return (
       <div>
-<<<<<<< HEAD
         <Navbar />
         <div className='top-content'>
           {this.props.children}
         </div>
-=======
-        <button onClick={account.SignOut} >logout</button>
-        {this.props.children}
->>>>>>> origin/master
       </div>
     )
   }
