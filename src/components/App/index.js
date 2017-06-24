861424f9209d7397cb8ac4cc1c3ec73234464a06
bukton <buk_ton2@hotmail.com>
2017-06-24 14:58:26 +0700

    icon app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <button onClick={ () => account.SignOut} >logout</button>
+        <button onClick={ () => account.SignOut() } >logout</button>
         {this.props.children}
       </div>
     )
