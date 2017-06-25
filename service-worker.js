aaa7f3ce64455433d0a19052afa1a7547419e694
bukton <buk_ton2@hotmail.com>
2017-06-25 14:35:44 +0700

    new sw

--- service-worker.js
+++ service-worker.js
@@ -6,22 +6,34 @@ workboxSW.router.registerRoute('/*', networkFirst)
 workboxSW.precache([
   {
     url: '/index.html',
-    revision: 'khj244',
+    revision: 'khj244kgfsdadsah1211dsks',
   },
   {
     url: '/manifest.json',
-    revision: '2ttfh2k4',
+    revision: 'fdg133rt1ertsdf35rer',
   },
   {
-    url: '/static/js/main.js',
-    revision: '2ttfh2k4uyxzjsdw',
+    url: '/static/js/main.f36ca39047bebbb4d9f0.js',
+    revision: '2ttfh2k4uyxzjerersdweef',
   },
   {
-    url: '/static/js/manifest.js',
-    revision: '2ttfh2k455fsvhj5dda',
+    url: '/static/js/manifest.f36ca39047bebbb4d9f0.js',
+    revision: '2ttfh2k455fsvhj5ddaff',
   }, 
   {
     url: '/static/asset/plugin/font-awesome/css/font-awesome.min.css',
-    revision: 'iyu222d',
+    revision: 'iyu25ywe6ddsdf22ed',
+  }, 
+  {
+    url: '/static/asset/images/384.png',
+    revision: 'iyu25ywe6ddf66jowwxc5dsdf22ed',
+  }, 
+  {
+    url: '/static/asset/images/HiewKao-logo-w-128.png',
+    revision: 'iyu25ywe6ddsdf22ed5er5wwf8wer5dfhjw',
+  }, 
+  {
+    url: '/static/asset/images/bg.jpg',
+    revision: 'iyu25ywe6ddsdf22ed5er5wwf8wer5dfhjw',
   }
 ])
